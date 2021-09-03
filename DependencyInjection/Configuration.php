e2483ce88955cfcd099cbdcf6614779ed1413d51
Alessandro GIULIANI <alessandro.giuliani67@gmail.com>
2021-09-03 10:41:06 +0200

    [UPDATE] Symfony 5.x compat

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,13 +17,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        if (method_exists(TreeBuilder::class, 'getRootNode')) {
-            $treeBuilder = new TreeBuilder('opensoft_rollout');
-            $rootNode = $treeBuilder->getRootNode();
-        } else {
-            $treeBuilder = new TreeBuilder();
-            $rootNode = $treeBuilder->root('opensoft_rollout');
-        }
+        $treeBuilder = new TreeBuilder('opensoft_rollout');
+        $rootNode = $treeBuilder->getRootNode();
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
